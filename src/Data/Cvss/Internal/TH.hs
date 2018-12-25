2755e7301554efe9c19bb90c9b870c08e146f564
Marcus Ofenhed <marcus@conditionraise.se>
2018-12-25 10:39:14 +0100

    Id works for subtypes.

--- src/Data/Cvss/Internal/TH.hs
+++ src/Data/Cvss/Internal/TH.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Data.Cvss.Internal.TH where
 
 import Language.Haskell.TH
-import Data.List (elemIndex)
+import Data.List (elemIndex, intercalate)
 
 --DataD
 --  :: Cxt
@@ -22,29 +23,56 @@ toLowerInitial orig@(a:rest) = case elemIndex a upper of
 
 dataDef:: String -> [(String, String, Bool, [(String, String)])] -> DecsQ
 dataDef topName types = do
-  d1 <- flip mapM types $ \(metricName, _, _, metricValues) ->
-          dataD
-            (return [])
-            (mkName metricName)
-            []
-            Nothing
-            (flip map metricValues $ \(name, short) -> normalC (mkName $ metricName ++ name) [])
-            [derivClause Nothing $ map (conT . mkName) ["Eq"]]
-  d2 <- dataD
-          (return [])
-          (mkName topName)
-          []
-          Nothing
-          [recC (mkName topName) $ flip map types $ \(metricName, metricShort, metricRequired, _) -> varBangType (mkName $ toLowerInitial metricName) $ bangType (return $ Bang NoSourceUnpackedness NoSourceStrictness) $ conT $ mkName metricName]
-          [derivClause Nothing $ map (conT . mkName) ["Show"]]
-  d3 <- flip mapM types $ \(metricName, metricShort, _, metricValues) ->
-          instanceD
-            (return [])
-            (appT (conT $ mkName "Show") $ conT $ mkName metricName)
-            -- [funD (mkName "show") [return $ Clause [VarP $ mkName "a"]
-            --                                 (NormalB $ LitE $ StringL $ metricShort)
-            --                                 []]]
-            [funD (mkName "show") $ flip map metricValues $ \(long, short) -> return $ Clause [ConP (mkName $ metricName ++ long) []]
-                                                                                              (NormalB $ LitE $ StringL $ metricShort ++ ":" ++ short)
-                                                                                              []]
-  return $ (d2:d1) ++ d3
+  f_intercalate <- runQ [| intercalate |]
+  f_readParen <- runQ [| readParen |]
+  ConE n_infix <- runQ [| (:) |]
+  createSubTypes <- flip mapM types $ \(metricName, _, _, metricValues) ->
+                      dataD
+                        (return [])
+                        (mkName metricName)
+                        []
+                        Nothing
+                        (flip map metricValues $ \(name, short) -> normalC (mkName $ metricName ++ name) [])
+                        [derivClause Nothing $ map (conT . mkName) ["Eq"]]
+  createTopType <- dataD
+                     (return [])
+                     (mkName topName)
+                     []
+                     Nothing
+                     [recC (mkName topName) $ flip map types $ \(metricName, metricShort, metricRequired, _) -> varBangType (mkName $ toLowerInitial metricName) $ bangType (return $ Bang NoSourceUnpackedness NoSourceStrictness) $ conT $ mkName metricName]
+                     [derivClause Nothing $ map (conT . mkName) []]
+  deriveShowSubs <- flip mapM types $ \(metricName, metricShort, _, metricValues) ->
+                      instanceD
+                        (return [])
+                        (appT (conT $ mkName "Show") $ conT $ mkName metricName)
+                        [funD (mkName "show") $ flip map metricValues $ \(long, short) -> return $ Clause [ConP (mkName $ metricName ++ long) []]
+                                                                                                          (NormalB $ LitE $ StringL $ metricShort ++ ":" ++ short)
+                                                                                                          []]
+  deriveShowTop <- do
+    varName <- newName "var"
+    instanceD
+      (return [])
+      (appT (conT $ mkName "Show") $ conT $ mkName topName)
+      [funD (mkName "show") [return $ Clause [VarP varName]
+                                             (NormalB $ AppE
+                                                          (AppE f_intercalate $ LitE $ StringL "/") $
+                                                          ListE $ flip map types $ \(metricName, metricShort, _, _) -> AppE (VarE $ mkName "show") $ AppE (VarE $ mkName $ toLowerInitial metricName) (VarE varName))
+                                             []]]
+  deriveReadSubs <- flip mapM types $ \(metricName, metricShort, _, metricValues) -> do
+                      depth <- newName "d" 
+                      instanceD
+                        (return [])
+                        (appT (conT $ mkName "Read") $ conT $ mkName metricName)
+                        [funD (mkName "readsPrec")
+                              [clause [varP depth]
+                                      (normalB $ appE (appE (return f_readParen) $ conE $ mkName "False") $
+                                                       lamCaseE $ flip map metricValues $ \(valueName, valueShort) -> do
+                                                                     restVar <- newName "rest" 
+                                                                     let str = metricShort ++ ":" ++ valueShort
+                                                                     match (return $ foldr (\c rest -> InfixP (LitP $ CharL c) n_infix rest) (VarP restVar) str)
+                                                                           (normalB $ listE [tupE [conE $ mkName $ metricName ++ valueName, varE restVar]])
+                                                                           [])
+                                      []]]
+  return $ (createTopType:createSubTypes)
+        ++ (deriveShowTop:deriveShowSubs)
+        ++ (deriveReadSubs)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,16 @@
 import Data.Cvss.V3
 
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Data.Monoid
+import Control.Monad
+
+idTest :: (Read a, Show a, Eq a) => a -> Assertion
+idTest v = v @?= (read $ show v)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMainWithOpts
+       [testCase "id-av:p" $ idTest AttackVectorPhysical
+       ,testCase "id-ac:h" $ idTest AttackComplexityHigh]
+       mempty
